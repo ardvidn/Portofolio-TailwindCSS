6a7b89bc044dbef8915ba48c18ab112857a1967a
Ardhani Virdian <ardvidn93@gmail.com>
2023-10-03 16:37:34 +0700

    add animation blob

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,22 @@ module.exports = {
       screen: {
         "2xl": "1320px",
       },
+      keyframes: {
+        blob: {
+          "0%": {
+            "border-radius": "60% 40% 30% 70%/60% 30% 70% 40%",
+          },
+          "50%": {
+            "border-radius": "30% 60% 70% 40%/50% 60% 30% 60%",
+          },
+          "100%": {
+            "border-radius": "60% 40% 30% 70%/60% 30% 70% 40%",
+          },
+        },
+      },
+      animation: {
+        blob: "blob 3s ease-in-out infinite",
+      },
       scale: {
         85: "0.85",
       },
